e5fe2c0724f556c012272322e5b2970e95f8b479
Daniel Garnica <thedaniel115@gmail.com>
2022-06-29 04:35:07 -0500

    Add News By Id Route

--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -14,3 +14,20 @@ router.get('/api/news', async (req, res) => {
         return res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+router.get('/api/news/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const keyWord = req.query.q || undefined;
+        const url = `https://newsapi.org/v2/everything?q=${keyWord}&apiKey=${process.env.NEWS_API_KEY}`;
+        const { data, status } = await axios.get(url);
+
+        const selectedNews = data.articles.find(
+            (news) => news.source.id === id
+        );
+        return res.status(status).json(selectedNews);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
